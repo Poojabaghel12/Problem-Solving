c0eefb9a5d5a7c5ee91e7dac987372e351e262a0
Poojabaghel12 <131783837+Poojabaghel12@users.noreply.github.com>
2024-09-01 23:39:50 +0530

    Time: 83 ms (53.42%), Space: 92.6 MB (37.03%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -7,11 +7,11 @@ public:
         }
         
         int idx = 0;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                result[i][j] = original[idx];
-                idx++;
-            }
+        for(int i=0; i<original.size(); i++){
+            int row = i/n;
+            int col = i%n;
+
+            result[row][col] = original[i];
         }
         return result;
     }
